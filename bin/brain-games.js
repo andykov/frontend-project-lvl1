23c8adb5e1028a5240f3f31aa1237ed4fdfe5828
andrey.b <abb.name@gmail.com>
2022-07-08 00:42:19 +0300

    fix: change variable name

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -2,5 +2,5 @@
 import getUserName from '../src/cli.js';
 
 console.log('Welcome to the Brain Games!');
-const name = getUserName();
-console.log(`Hello, ${name}!`);
+const username = getUserName();
+console.log(`Hello, ${username}!`);
