c94c75bea8ccfa16de00319ab3e305a49da4e7d2
andrey.b <abb.name@gmail.com>
2022-07-11 23:31:21 +0300

    fix: code refactoring

--- src/cli.js
+++ src/cli.js
@@ -1,11 +1,5 @@
-import readlineSync from 'readline-sync';
-
-// console.log('Welcome to the Brain Games!');
-
-function getUserName() {
-  const message = 'May I have your name? ';
-  const username = readlineSync.question(message);
-  return username || getUserName();
+function msgWelcome() {
+  console.log('Welcome to the Brain Games!');
 }
 
-export default getUserName;
+export default msgWelcome;
